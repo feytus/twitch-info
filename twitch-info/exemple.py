13d950cc552fda436cd2746b5152e9b805a646a4
Feytus <35037869+feytus@users.noreply.github.com>
2021-09-21 21:45:56 +0200

    Update exemple.py

--- twitch-info/exemple.py
+++ twitch-info/exemple.py
@@ -3,9 +3,9 @@ from __init__ import twitch_info
 twitch = twitch_info(client_id="<YOUR CLIENT ID>", 
                     acces_token="<YOUR CLIENT ID>")
 
-user_id = twitch.get_user_id(user_name="<CHANNEL NAME>")
+user_id: int = twitch.get_user_id(user_name="<CHANNEL NAME>")
 
-user_info = twitch.get_stream(user_id=user_id)
+user_info: dict = twitch.get_stream(user_id=user_id)
 
-checking = twitch.check_for_stream(user="daiko")
+is_in_live: bool = twitch.check_for_stream(user="daiko")
 
